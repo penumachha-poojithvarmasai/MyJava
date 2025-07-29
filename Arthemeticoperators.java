3f3eb43f1c07853af4208355b187912b4a2e32cb
Poojith varma sai Penumachha <poojithpenumachha14@gmail.com>
2025-07-29 13:35:44 +0530

    Add files via upload

---
+++ Arthemeticoperators.java
@@ -0,0 +1,26 @@
+package myjava;
+
+public class Arthemeticoperators {
+
+	public static void main(String[] args) {
+		int x=10,y=2;
+		
+		//addition
+		System.out.println("Sum is "+(x+y));
+		
+		//subtraction
+		System.out.println("Subtract is "+(x-y));
+		
+		//Multiplication
+		System.out.println("Multiply is "+(x*y));
+		
+		//Division
+		System.out.println("Division is "+(x/y));
+		
+		//Remainder
+		System.out.println("Mod is "+(x%y));
+		
+
+	}
+
+}
